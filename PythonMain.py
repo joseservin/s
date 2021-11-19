3e91083cb8d204b4919c6895bf2d2d9129dabd0d
joseservin <94584175+joseservin@users.noreply.github.com>
2021-11-19 09:23:20 -0600

    Add files via upload

---
+++ PythonMain.py
@@ -0,0 +1,14 @@
+
+"Hello World"
+
+class Galleta:
+
+    def __init__(self):
+        print("Hola, soy un nuevo objeto")
+    def instanciar(self):
+        print("instancia")
+
+A=Galleta()
+A2=Galleta()
+
+        
